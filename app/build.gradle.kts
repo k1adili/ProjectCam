c4c1620247b637332c992ec93045bb591a0df84c
Keyvan Adili <66829528+k1adili@users.noreply.github.com>
2026-08-29 12:43:32 +0330

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ir.k1adili.projectcam"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
